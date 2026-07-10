fb594f8f4d67a218e7e1f0e260f33468da4e589a
azoel1 <azoel1@outlook.com>
2026-07-10 19:27:59 +0300

    Update main.cpp

--- source/main.cpp
+++ source/main.cpp
@@ -3,13 +3,10 @@
 #include <string.h>
 #include <stdlib.h>
 
-// Основные заголовки libnx для графики и ввода уже включены через #include <switch.h>
-
 #define MAZE_W 15
 #define MAZE_H 9
 
 const char* maze_levels[10][MAZE_H] = {
-    // ... (оставь свой массив уровней здесь без изменений)
     { "###############", "#S...#......F.#", "###.#.###.#.###", "#.....#.#.#...#", "#.#####.#####.#", "#...#.......#.#", "###.#.###.#.###", "#.....#.#.#...#", "###############" },
     { "###############", "#S#...#...#...#", "#.#.#.#.#.#.#.#", "#...#...#...#.#", "#######.#####.#", "#...........#.#", "#.#########.#.#", "#...........F#", "###############" },
     { "###############", "#S....#.......#", "#####.#.#######", "#...#.#.#.....#", "#.#####.#####.#", "#.#####.###.#.#", "#.#####.....#.#", "#F###########.#", "###############" },
@@ -23,15 +20,12 @@ const char* maze_levels[10][MAZE_H] = {
 };
 
 int main(int argc, char** argv) {
-    // Инициализация графики и консоли
-    gfxInitDefault();
+    // Инициализация консоли — это все, что нужно для вывода в libnx
     consoleInit(NULL);
 
     int current_level = 0;
     int player_x = 1, player_y = 1;
-    bool scream_triggered = false;
 
-    // Настройка контроллера
     padConfigureInput(1, HidNpadStyleSet_NpadStandard);
     PadState pad;
     padInitializeDefault(&pad);
@@ -42,39 +36,36 @@ int main(int argc, char** argv) {
 
         if (kDown & HidNpadButton_Plus) break;
 
-        if (!scream_triggered) {
-            if (kDown & (HidNpadButton_Up | HidNpadButton_Down | HidNpadButton_Left | HidNpadButton_Right)) {
-                int next_x = player_x;
-                int next_y = player_y;
+        // Логика движения: ровно одна клетка на нажатие
+        if (kDown & (HidNpadButton_Up | HidNpadButton_Down | HidNpadButton_Left | HidNpadButton_Right)) {
+            int next_x = player_x;
+            int next_y = player_y;
 
-                if (kDown & HidNpadButton_Up)    next_y--;
-                if (kDown & HidNpadButton_Down)  next_y++;
-                if (kDown & HidNpadButton_Left)  next_x--;
-                if (kDown & HidNpadButton_Right) next_x++;
+            if (kDown & HidNpadButton_Up)    next_y--;
+            else if (kDown & HidNpadButton_Down)  next_y++;
+            else if (kDown & HidNpadButton_Left)  next_x--;
+            else if (kDown & HidNpadButton_Right) next_x++;
 
-                if (next_x >= 0 && next_x < MAZE_W && next_y >= 0 && next_y < MAZE_H) {
-                    if (maze_levels[current_level][next_y][next_x] != '#') {
-                        player_x = next_x;
-                        player_y = next_y;
-                    }
+            if (next_x >= 0 && next_x < MAZE_W && next_y >= 0 && next_y < MAZE_H) {
+                if (maze_levels[current_level][next_y][next_x] != '#') {
+                    player_x = next_x;
+                    player_y = next_y;
                 }
-            }
-
-            consoleClear();
-            for(int i=0; i<MAZE_H; i++) {
-                for(int j=0; j<MAZE_W; j++) {
-                    if(i == player_y && j == player_x) printf("O");
-                    else printf("%c", maze_levels[current_level][i][j]);
-                }
-                printf("\n");
             }
         }
 
-        gfxFlushBuffers();
-        gfxSwapBuffers();
-        gfxWaitForVsync();
+        // Вывод на экран
+        consoleClear();
+        for(int i=0; i<MAZE_H; i++) {
+            for(int j=0; j<MAZE_W; j++) {
+                if(i == player_y && j == player_x) printf("O");
+                else printf("%c", maze_levels[current_level][i][j]);
+            }
+            printf("\n");
+        }
+        
+        consoleUpdate(NULL);
     }
 
-    gfxExit();
     return 0;
 }
